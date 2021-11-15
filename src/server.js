e344244553d3c0444ba178a325c6610a861fc55b
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-15 12:02:15 +0100

    mongo connection established

--- src/server.js
+++ src/server.js
@@ -1,3 +1,26 @@
 import express from "express";
 import list from "express-list-endpoints";
+import mongoose from "mongoose";
 import cors from "cors";
+
+const server = express();
+
+const port = process.env.PORT | 3001;
+// ******************** MIDDLEWARES *************************+
+
+server.use(cors());
+server.use(express.json());
+// ******************** ROUTES ******************************
+
+// ********************** ERROR HANDLERS *************************
+
+console.table(list(server));
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("database Connected");
+  server.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+});
