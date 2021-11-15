56b27abf419340144cba8865d2f1d3f694dd0e83
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-15 12:48:39 +0100

    update

---
+++ src/users/schema.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const { model, Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  refreshToken: {
+    type: String,
+  },
+  googleId: {
+    type: String,
+  },
+});
+
+export default model("user", userSchema);
