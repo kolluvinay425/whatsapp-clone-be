fbbca23149b45377c58fbe193decea0bccf56d4f
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-16 13:39:09 +0100

    update

--- src/chat/index.js
+++ src/chat/index.js
@@ -1,32 +1,37 @@
-import { server } from "../../server.js";
-// import { createServer } from "http";
-// import { Server } from "socket.io";
+import server from "../server";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
 
-// const chatServer = createServer(server);
+const chatServer = createServer(server);
+const port = process.env.PORT || 3001;
+const io = new Server(chatServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-// const io = new Server(chatServer, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
+  socket.on("join_room", (roomId, username) => {
+    socket.join(roomId);
+    console.log(`userName with ${username} joined room:${roomId}`);
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.log("message:", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
 
-//   socket.on("join_room", (roomId, username) => {
-//     socket.join(roomId);
-//     console.log(`userName with ${username} joined room:${roomId}`);
-//   });
-//   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//     console.log("message:", data);
-//   });
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected", socket.id);
-//   });
-// });
-
-// server.listen(port, () => {
-//   console.log(`running on ${port}`);
-// });
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on("connected", () => {
+  console.log("database connected");
+});
+server.listen(port, () => {
+  console.log(`running on ${port}`);
+});
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import list from "express-list-endpoints";
 import cors from "cors";
 import userRouter from "./users/index.js";
-import mongoose from "mongoose";
+
 import {
   unauthorizedHandler,
   forbiddenHandler,
@@ -25,38 +25,31 @@ console.table(list(server));
 
 //---------------------------------------socket.io-----------------------------------------------
 
-import { createServer } from "http";
-import { Server } from "socket.io";
+// import { createServer } from "http";
+// import { Server } from "socket.io";
 
-const chatServer = createServer(server);
-const port = process.env.PORT || 3001;
-const io = new Server(chatServer, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-  },
-});
+// const chatServer = createServer(server);
+//
+// const io = new Server(chatServer, {
+//   cors: {
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST"],
+//   },
+// });
 
-io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
+// io.on("connection", (socket) => {
+//   console.log("User connected:", socket.id);
 
-  socket.on("join_room", (roomId, username) => {
-    socket.join(roomId);
-    console.log(`userName with ${username} joined room:${roomId}`);
-  });
-  socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data);
-    console.log("message:", data);
-  });
-  socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id);
-  });
-});
-// export default server;
-mongoose.connect(process.env.MONGO_URL);
-mongoose.connection.on("connected", () => {
-  console.log("database connected");
-});
-server.listen(port, () => {
-  console.log(`running on ${port}`);
-});
+//   socket.on("join_room", (roomId, username) => {
+//     socket.join(roomId);
+//     console.log(`userName with ${username} joined room:${roomId}`);
+//   });
+//   socket.on("send_message", (data) => {
+//     socket.to(data.room).emit("receive_message", data);
+//     console.log("message:", data);
+//   });
+//   socket.on("disconnect", () => {
+//     console.log("user disconnected", socket.id);
+//   });
+// });
+export default server;
