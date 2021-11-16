0be7612c33af3e6c376bfa5c1a52e463a5dc4ecc
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-16 13:01:50 +0100

    update

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import list from "express-list-endpoints";
 import cors from "cors";
 import userRouter from "./users/index.js";
+import mongoose from "mongoose";
 import {
   unauthorizedHandler,
   forbiddenHandler,
@@ -22,4 +23,40 @@ server.use(catchAllHandler);
 
 console.table(list(server));
 
-export default server;
+//---------------------------------------socket.io-----------------------------------------------
+
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const chatServer = createServer(server);
+const port = process.env.PORT || 3001;
+const io = new Server(chatServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("join_room", (roomId, username) => {
+    socket.join(roomId);
+    console.log(`userName with ${username} joined room:${roomId}`);
+  });
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.log("message:", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected", socket.id);
+  });
+});
+// export default server;
+mongoose.connect(process.env.MONGO_URL);
+mongoose.connection.on("connected", () => {
+  console.log("database connected");
+});
+server.listen(port, () => {
+  console.log(`running on ${port}`);
+});
