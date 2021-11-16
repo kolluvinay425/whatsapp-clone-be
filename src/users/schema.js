a9032979710f32aa369b22b4c9f474f7666f68ef
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-16 10:14:22 +0100

    update

--- src/users/schema.js
+++ src/users/schema.js
@@ -15,6 +15,11 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  avatar: {
+    type: String,
+    default:
+      "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png",
+  },
   refreshToken: {
     type: String,
   },
