901a3849f799c5b6324adcd0499a4824096e5cbd
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-18 14:49:35 +0100

    update

--- src/server.js
+++ src/server.js
@@ -22,34 +22,3 @@ server.use(forbiddenHandler);
 server.use(catchAllHandler);
 
 console.table(list(server));
-
-//---------------------------------------socket.io-----------------------------------------------
-
-// import { createServer } from "http";
-// import { Server } from "socket.io";
-
-// const chatServer = createServer(server);
-//
-// const io = new Server(chatServer, {
-//   cors: {
-//     origin: "http://localhost:3000",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
-// io.on("connection", (socket) => {
-//   console.log("User connected:", socket.id);
-
-//   socket.on("join_room", (roomId, username) => {
-//     socket.join(roomId);
-//     console.log(`userName with ${username} joined room:${roomId}`);
-//   });
-//   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//     console.log("message:", data);
-//   });
-//   socket.on("disconnect", () => {
-//     console.log("user disconnected", socket.id);
-//   });
-// });
-export default server;
