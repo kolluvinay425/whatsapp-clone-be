45b13c218cfdfbb543239ff1152b3ad62e20c0d7
kolluvinay425 <kolluvinay425@gmail.com>
2021-11-18 14:57:26 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 import express from "express";
 import list from "express-list-endpoints";
+import mongoose from "mongoose";
 import cors from "cors";
 import userRouter from "./users/index.js";
-
 import {
   unauthorizedHandler,
   forbiddenHandler,
@@ -10,6 +10,7 @@ import {
 } from "./errorHandlers.js";
 const server = express();
 
+export const port = process.env.PORT || 3001;
 // ******************** MIDDLEWARES *************************+
 
 server.use(cors());
@@ -22,3 +23,14 @@ server.use(forbiddenHandler);
 server.use(catchAllHandler);
 
 console.table(list(server));
+
+mongoose.connect(process.env.MONGO_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log("database Connected");
+  server.listen(port, () => {
+    console.log(`server running on port ${port}`);
+  });
+});
+
+export default server;
